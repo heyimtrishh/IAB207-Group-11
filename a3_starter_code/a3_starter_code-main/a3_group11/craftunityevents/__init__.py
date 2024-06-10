59413e53193fef32d162c1655db46c0bac1cbd30
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-10 18:22:31 +1000

    Update __init__.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
@@ -1,47 +1,61 @@
-#import flask - from the package import class
-from flask import Flask 
+from flask import Flask, render_template
 from flask_bootstrap import Bootstrap5
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
+from flask_bcrypt import Bcrypt
+import datetime
 
 db = SQLAlchemy()
 
-#create a function that creates a web application
-# a web server will run this web application
 def create_app():
-  
-    app = Flask(__name__)  # this is the name of the module/package that is calling this app
-    # Should be set to false in a production environment
-    app.debug = True
-    app.secret_key = 'somesecretkey'
-    #set the app configuration data 
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
-    #initialize db with flask app
+    app = Flask(__name__)
+    #we use this utility module to display forms quickly
+    Bootstrap5(app)
+
+    #this is a much safer way to store passwords
+    Bcrypt(app)
+
+    #a secret key for the session object
+    #(it would be better to use an environment variable here)
+    app.secret_key = 'somerandomvalue'
+
+    #Configue and initialise DB
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
     db.init_app(app)
 
-    Bootstrap5(app)
+    #config upload folder
+    UPLOAD_FOLDER = '/static/image'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
     
-    #initialize the login manager
+    #initialise the login manager
     login_manager = LoginManager()
-    
-    # set the name of the login function that lets user login
-    # in our case it is auth.login (blueprintname.viewfunction name)
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    # create a user loader function takes userid and returns User
-    # Importing inside the create_app function avoids circular references
-    from .models import User
+    #create a user loader function takes userid and returns User
+    from .models import User  # importing here to avoid circular references
     @login_manager.user_loader
     def load_user(user_id):
-       return User.query.get(int(user_id))
+        return User.query.get(int(user_id))
 
-    #importing views module here to avoid circular references
-    # a commonly used practice.
+    #add Blueprints
     from . import views
-    app.register_blueprint(views.main_bp)
-
+    app.register_blueprint(views.mainbp)
+    from . import destinations
+    app.register_blueprint(destinations.destbp)
     from . import auth
-    app.register_blueprint(auth.auth_bp)
-    
+    app.register_blueprint(auth.authbp)
+
+    @app.errorhandler(404) 
+    # inbuilt function which takes error as parameter 
+    def not_found(e): 
+      return render_template("404.html", error=e)
+
+    #this creates a dictionary of variables that are available
+    #to all html templates
+    @app.context_processor
+    def get_context():
+      year = datetime.datetime.today().year
+      return dict(year=year)
+
     return app
