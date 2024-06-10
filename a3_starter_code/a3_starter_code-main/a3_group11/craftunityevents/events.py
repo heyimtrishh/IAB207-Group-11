4ef72af2bc2eaf7332d684b18fb9681c380dc010
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-10 18:38:07 +1000

    Create events.py

---
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/events.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, render_template, request, redirect, url_for, flash
+from .models import Event, Comment
+from .forms import EventForm, CommentForm
+from . import db
+import os
+from werkzeug.utils import secure_filename
+#additional import:
+from flask_login import login_required, current_user
+
+destbp = Blueprint('event', __name__, url_prefix='/events')
+
+@destbp.route('/<id>')
+def show(id):
+    event = db.session.scalar(db.select(Event).where(Event.id==id))
+    # create the comment form
+    form = CommentForm()    
+    return render_template('events/show.html', event=event, form=form)
+
+@destbp.route('/create', methods=['GET', 'POST'])
+@login_required
+def create():
+  print('Method type: ', request.method)
+  form = DestinationForm()
+  if form.validate_on_submit():
+    #call the function that checks and returns image
+    db_file_path = check_upload_file(form)
+    event = Event(name=form.name.data,event=form.event.data, 
+    image=db_file_path,currency=form.currency.data)
+    # add the object to the db session
+    db.session.add(event)
+    # commit to the database
+    db.session.commit()
+    flash('Successfully created new travel event', 'success')
+    #Always end with redirect when form is valid
+    return redirect(url_for('event.create'))
+  return render_template('events/create.html', form=form)
+
+def check_upload_file(form):
+  #get file data from form  
+  fp = form.image.data
+  filename = fp.filename
+  #get the current path of the module file… store image file relative to this path  
+  BASE_PATH = os.path.dirname(__file__)
+  #upload file location – directory of this file/static/image
+  upload_path = os.path.join(BASE_PATH, 'static/image', secure_filename(filename))
+  #store relative path in DB as image location in HTML is relative
+  db_upload_path = '/static/image/' + secure_filename(filename)
+  #save the file and return the db upload path
+  fp.save(upload_path)
+  return db_upload_path
+
+@destbp.route('/<id>/comment', methods=['GET', 'POST'])  
+@login_required
+def comment(id):  
+    form = CommentForm()  
+    #get the destination object associated to the page and the comment
+    event = db.session.scalar(db.select(Event).where(Event.id==id))
+    if form.validate_on_submit():  
+      #read the comment from the form
+      comment = Comment(text=form.text.data, event=event,
+                        user=current_user) 
+      #here the back-referencing works - comment.event is set
+      # and the link is created
+      db.session.add(comment) 
+      db.session.commit() 
+      #flashing a message which needs to be handled by the html
+      flash('Your comment has been added', 'success')  
+      # print('Your comment has been added', 'success') 
+    # using redirect sends a GET request to event.show
+    return redirect(url_for('event.show', id=id))
