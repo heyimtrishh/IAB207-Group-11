ca41bcdfd1f907470e9b3aa218a1c2a292dec8ee
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-10 18:25:48 +1000

    Update views.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/views.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/views.py
@@ -1,7 +1,20 @@
-from flask import Blueprint
+from flask import Blueprint, render_template, request, redirect, url_for
+from .models import Destination
+from . import db
 
-main_bp = Blueprint('main', __name__)
+mainbp = Blueprint('main', __name__)
 
-@main_bp.route('/')
+@mainbp.route('/')
 def index():
-    return '<h1>Starter code for the assessment<h1>'
+    event = db.session.scalars(db.select(Event)).all()    
+    return render_template('index.html', events=events)
+
+@mainbp.route('/search')
+def search():
+    if request.args['search'] and request.args['search'] != "":
+        print(request.args['search'])
+        query = "%" + request.args['search'] + "%"
+        event = db.session.scalars(db.select(Event).where(Event.description.like(query)))
+        return render_template('index.html', events=events)
+    else:
+        return redirect(url_for('main.index'))
