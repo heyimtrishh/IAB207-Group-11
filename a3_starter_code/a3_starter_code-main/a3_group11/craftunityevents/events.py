9b16cf8fad5c3447e5a03772186e32db14a7784f
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-11 17:20:03 +1000

    Update events.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/events.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/events.py
@@ -1,123 +1,89 @@
 from flask import Blueprint, render_template, request, redirect, url_for, flash
 from .models import Event, Comment
-from .forms import EventForm, CommentForm
+from .forms import EventForm, CommentForm, UpdateEventForm, OrderForm
 from . import db
 import os
 from werkzeug.utils import secure_filename
-#additional import:
 from flask_login import login_required, current_user
+from datetime import datetime
 
 destbp = Blueprint('event', __name__, url_prefix='/events')
 
 @destbp.route('/<id>')
 def show(id):
-    event = db.session.scalar(db.select(Event).where(Event.id==id))
+    event = db.session.scalar(db.select(Event).where(Event.id == id))
     # create the comment form
-    form = CommentForm()    
+    form = CommentForm()
     return render_template('events/show.html', event=event, form=form)
 
 # Create Event
 @destbp.route('/create', methods=['GET', 'POST'])
 @login_required
 def create():
-  print('Method type: ', request.method)
-  form = EventForm()
-  if form.validate_on_submit():
-      
-    #call the function that checks and returns image
-    db_file_path = check_upload_file(form)
-    event = Event(
-        event_name=form.name.data,
-        category=form.category.data,
-        start_time=form.start_time.data,
-        end_time=form.end_time.data,
-        start_date=form.start_date.data,
-        end_date=form.end_date.data,
-        location=form.location.data,
-        image=db_file_path,
-        description=form.description.data,
-        price=form.price.data,
-        num_tickets=form.num_tickets.data,
-        created_by=current_user.id
-    )
-      
-    # add the object to the db session
-    db.session.add(event)
-      
-    # commit to the database
-    db.session.commit()
-    flash(f'Successfully created new event for {event.event_name}!', 'success')
-      
-    #Always end with redirect when form is valid
-    return redirect(url_for('event.create'))
-  return render_template('events/create.html', form=form)
+    print('Method type: ', request.method)
+    form = EventForm()
+    if form.validate_on_submit():
+        # call the function that checks and returns image
+        db_file_path = check_upload_file(form)
+        event = Event(
+            event_name=form.event_name.data,
+            category=form.event_category.data,
+            start_time=form.start_time.data,
+            end_time=form.end_time.data,
+            start_date=form.start_date.data,
+            end_date=form.end_date.data,
+            location=form.event_location.data,
+            image=db_file_path,
+            description=form.event_description.data,
+            price=form.ticket_price.data,
+            num_tickets=form.ticket_quantity.data,
+            created_by=current_user.id
+        )
+        # add the object to the db session
+        db.session.add(event)
+        # commit to the database
+        db.session.commit()
+        flash(f'Successfully created new event for {event.event_name}!', 'success')
+        # Always end with redirect when form is valid
+        return redirect(url_for('event.create'))
+    return render_template('events/create.html', form=form)
 
 # Update Event as Event Creator
-@destbp.route('/<id>/comment', methods=['GET', 'POST'])  
+@destbp.route('/<id>/update', methods=['GET', 'POST'])
 @login_required
 def update_event(id):
     event = Event.query.get_or_404(id)
-
-    # Check if the current user is the creator of the event
-    if event.user_id != current_user.id:
-        flash('You do not have permission to edit this event.')
+    # Only allow the event creator to edit this event
+    if event.created_by != current_user.id:
+        flash('Sorry, you do not have permission to edit this event.')
         return redirect(url_for('event.show', id=id))
 
-    form = EventForm(obj=event)  # Pre-populate the form with the event's data
-
+    form = UpdateEventForm(obj=event)
     if form.validate_on_submit():
-        update_event_from_form(event, form)
-        if save_event(event):
-            flash('Event updated successfully.')
-            return redirect(url_for('main.index'))
-        else:
-            flash('An error occurred. Event update failed.')
-    else:
-        print(form.errors)
-
-    return render_template('events/updateEvent.html', form=form, event=event)
-
-def update_event_from_form(event, form):
-    """Update event object with data from the form."""
-    event.name = form.name.data
-    event.location = form.location.data
-    event.start_date = form.start_date.data
-    event.end_date = form.end_date.data
-    event.start_time = form.start_time.data
-    event.end_time = form.end_time.data
-    event.ga_price = form.ga_price.data
-    event.ga_availability = form.ga_availability.data
-    event.concession_price = form.concession_price.data
-    event.concession_availability = form.concession_availability.data
-    event.vip_price = form.vip_price.data
-    event.vip_availability = form.vip_availability.data
-    event.description = form.description.data
-    if form.image.data:  # Check if the image file has been updated
-        new_image_filename = save_image(form.image.data)
-        if new_image_filename:
-            event.image = new_image_filename
-    event.category = form.category.data
-    event.event_guidelines = form.event_guidelines.data
-    event.terms_conditions = form.terms_conditions.data
-
-
-def save_event(event):
-    """Attempt to save the event to the database."""
-    try:
+        event.event_name = form.event_name.data
+        event.category = form.event_category.data
+        event.start_time = form.start_time.data
+        event.end_time = form.end_time.data
+        event.start_date = form.start_date.data
+        event.end_date = form.end_date.data
+        event.location = form.event_location.data
+        if form.image.data:
+            db_file_path = check_upload_file(form)
+            event.image = db_file_path
+        event.description = form.event_description.data
+        event.price = form.ticket_price.data
+        event.num_tickets = form.ticket_quantity.data
         db.session.commit()
-        print("Successful update!")
-        return True
-    except Exception as e:
-        print("Error updating event:", e)
-        db.session.rollback()
-        return False
+        flash('Your event has been updated!', 'success')
+        return redirect(url_for('event.show', id=id))
+
+    return render_template('events/update.html', form=form, event=event)
 
 # Cancel Event
 @destbp.route('/cancel/<int:id>', methods=['POST'])
 @login_required
 def cancel_event(id):
     event = Event.query.get_or_404(id)
-                        
     # Only Event Creator Can Cancel Event
     if event.created_by != current_user.id:
         flash('Sorry, you do not have permission to cancel this event.')
@@ -129,18 +95,18 @@ def cancel_event(id):
 
 # File Upload
 def check_upload_file(form):
-  #get file data from form  
-  fp = form.image.data
-  filename = fp.filename
-  #get the current path of the module file… store image file relative to this path  
-  BASE_PATH = os.path.dirname(__file__)
-  #upload file location – directory of this file/static/image
-  upload_path = os.path.join(BASE_PATH, 'static/image', secure_filename(filename))
-  #store relative path in DB as image location in HTML is relative
-  db_upload_path = '/static/image/' + secure_filename(filename)
-  #save the file and return the db upload path
-  fp.save(upload_path)
-  return db_upload_path
+    # get file data from form  
+    fp = form.image.data
+    filename = secure_filename(fp.filename)
+    # get the current path of the module file… store image file relative to this path  
+    BASE_PATH = os.path.dirname(__file__)
+    # upload file location – directory of this file/static/image
+    upload_path = os.path.join(BASE_PATH, 'static/image', filename)
+    # store relative path in DB as image location in HTML is relative
+    db_upload_path = '/static/image/' + filename
+    # save the file and return the db upload path
+    fp.save(upload_path)
+    return db_upload_path
 
 # Book Event
 @destbp.route('/<int:id>/book', methods=['GET', 'POST'])
@@ -167,25 +133,24 @@ def book_event(id):
     return render_template('events/book.html', form=form, event=event)
 
 # Comment on Event Details Page
-@destbp.route('/<id>/comment', methods=['GET', 'POST'])  
+@destbp.route('/<id>/comment', methods=['GET', 'POST'])
 @login_required
-def comment(id):  
-    form = CommentForm()  
-    #get the event object associated to the page and the comment
-    event = db.session.scalar(db.select(Event).where(Event.id==id))
-    if form.validate_on_submit():  
-      #read the comment from the form
-      comment = Comment(text=form.text.data, event=event,
-                        user=current_user) 
-      #here the back-referencing works - comment.event is set
-      # and the link is created
-      db.session.add(comment) 
-      db.session.commit() 
-      #flashing a message which needs to be handled by the html
-      flash('Your comment has been added!', 'success')  
-      # print('Your comment has been added!', 'success') 
-    # using redirect sends a GET request to event.show
-    return redirect(url_for('event.show', id=id))
+def comment(id):
+    form = CommentForm()
+    # get the event object associated to the page and the comment
+    event = db.session.scalar(db.select(Event).where(Event.id == id))
+    if form.validate_on_submit():
+        # read the comment from the form
+        comment = Comment(text=form.text.data, event=event, user=current_user)
+        # here the back-referencing works - comment.event is set
+        # and the link is created
+        db.session.add(comment)
+        db.session.commit()
+        # flashing a message which needs to be handled by the html
+        flash('Your comment has been added!', 'success')
+        # using redirect sends a GET request to event.show
+        return redirect(url_for('event.show', id=id))
+    return render_template('events/show.html', event=event, form=form)
 
 # Error Handlers
 @destbp.errorhandler(404)
