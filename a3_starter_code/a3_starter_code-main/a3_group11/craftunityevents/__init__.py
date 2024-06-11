7dd97d7640c2d5f98b47cad19f6bf7ec9f0272b2
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-11 18:00:50 +1000

    Update __init__.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
@@ -1,61 +1,31 @@
-from flask import Flask, render_template
-from flask_bootstrap import Bootstrap5
+from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_bcrypt import Bcrypt
-import datetime
+from flask_bootstrap import Bootstrap5
 
 db = SQLAlchemy()
 
 def create_app():
     app = Flask(__name__)
-    #we use this utility module to display forms quickly
+    app.debug = True
+    app.secret_key = 'somesecretkey'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///sitedata.sqlite'
+    db.init_app(app)
     Bootstrap5(app)
 
-    #this is a much safer way to store passwords
-    Bcrypt(app)
-
-    #a secret key for the session object
-    #(it would be better to use an environment variable here)
-    app.secret_key = 'somerandomvalue'
-
-    #Configue and initialise DB
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///traveldb.sqlite'
-    db.init_app(app)
-
-    #config upload folder
-    UPLOAD_FOLDER = '/static/image'
-    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER 
-    
-    #initialise the login manager
     login_manager = LoginManager()
     login_manager.login_view = 'auth.login'
     login_manager.init_app(app)
 
-    #create a user loader function takes userid and returns User
-    from .models import User  # importing here to avoid circular references
+    from .models import User
     @login_manager.user_loader
     def load_user(user_id):
         return User.query.get(int(user_id))
 
-    #add Blueprints
-    from . import views
-    app.register_blueprint(views.mainbp)
-    from . import events
-    app.register_blueprint(events.destbp)
-    from . import auth
-    app.register_blueprint(auth.authbp)
+    from .views import main_bp
+    app.register_blueprint(main_bp)
 
-    @app.errorhandler(404) 
-    # inbuilt function which takes error as parameter 
-    def not_found(e): 
-      return render_template("404.html", error=e)
-
-    #this creates a dictionary of variables that are available
-    #to all html templates
-    @app.context_processor
-    def get_context():
-      year = datetime.datetime.today().year
-      return dict(year=year)
-
+    from .auth import auth_bp
+    app.register_blueprint(auth_bp)
+    
     return app
