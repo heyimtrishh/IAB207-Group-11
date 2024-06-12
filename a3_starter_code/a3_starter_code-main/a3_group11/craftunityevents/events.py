13f7d339a3881e05c3126039004d7f745c5bb9ab
Nina Chung <nina_chung12@hotmail.com>
2024-06-12 16:32:23 +1000

    changes in events.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/events.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/events.py
@@ -54,7 +54,7 @@ def create():
         flash(f'Successfully created new event for {event.event_name}!', 'success')
         # Always end with redirect when form is valid
         return redirect(url_for('event.create'))
-    return render_template('events/create.html', form=form)
+    return render_template('events/create_event.html', form=form)
 
 # Update Event as Event Creator
 @destbp.route('/<id>/update', methods=['GET', 'POST'])
