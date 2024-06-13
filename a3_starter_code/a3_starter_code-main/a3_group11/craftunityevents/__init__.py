f0ddf99fbad3788230dd3e6e08b4d21c9f0a6991
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-13 12:58:44 +1000

    update init.py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/__init__.py
@@ -23,12 +23,12 @@ def create_app():
         return User.query.get(int(user_id))
 
     from .views import mainbp
-    app.register_blueprint(mainbp)
+    app.register_blueprint(mainbp, url_prefix='/')
 
     from .auth import authbp
-    app.register_blueprint(authbp)
+    app.register_blueprint(authbp, url_prefix='/auth')
 
     from .events import destbp as events_blueprint
-    app.register_blueprint(events_blueprint)
+    app.register_blueprint(events_blueprint, url_prefix='/events')
 
     return app
