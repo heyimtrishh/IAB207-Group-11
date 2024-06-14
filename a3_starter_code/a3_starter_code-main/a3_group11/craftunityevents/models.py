a8771f57ee362c55e99200910414df8effb7ad36
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-14 16:43:27 +1000

    revert models py

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/models.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/models.py
@@ -51,7 +51,6 @@ class Comment(db.Model):
     event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
 
-    user = db.relationship('User', backref='comments', lazy=True)
 
     def __repr__(self):
         return f"Comment: {self.text}"
