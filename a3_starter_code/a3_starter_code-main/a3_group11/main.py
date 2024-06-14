c8b334fb232435fbcdc4854e594105c038c53e54
Trish <169013267+heyimtrishh@users.noreply.github.com>
2024-06-14 14:37:43 +1000

    update main

--- a3_starter_code/a3_starter_code-main/a3_group11/main.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/main.py
@@ -1,7 +1,5 @@
-import logging
 from craftunityevents import create_app
 
 if __name__ == '__main__':
-    logging.basicConfig(filename='error.log', level=logging.DEBUG)
     app = create_app()
     app.run(debug=True)
