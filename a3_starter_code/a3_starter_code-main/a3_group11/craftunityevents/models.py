1bfb828c1b8e5bb2bda3e524a2e313d6cf42e9f2
DaltonBernard99 <162665856+DaltonBernard99@users.noreply.github.com>
2024-06-14 22:03:11 +1000

    Update models.py
    adding comments

--- a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/models.py
+++ a3_starter_code/a3_starter_code-main/a3_group11/craftunityevents/models.py
@@ -1,86 +1,95 @@
-from . import db
-from datetime import datetime
-from flask_login import UserMixin
+# Importing necessary modules
+from . import db  # Import the database instance from the current package
+from datetime import datetime  # Import datetime module for handling date and time
+from flask_login import UserMixin  # Import UserMixin for user session management
 
+# Define the User model with UserMixin for session management
 class User(db.Model, UserMixin):
-    __tablename__ = 'users'
-    id = db.Column(db.Integer, primary_key=True)
-    full_name = db.Column(db.String(100), index=True, unique=True, nullable=False)
-    contact_number = db.Column(db.Integer, index=True, nullable=False)
-    email_id = db.Column(db.String(100), index=True, nullable=False)
-    address = db.Column(db.String(100), index=True, nullable=False)
-    password_hash = db.Column(db.String(400), nullable=False)
-    
-    # Relationships
-    comments = db.relationship('Comment', backref='user', lazy=True)
-    bookings = db.relationship('Booking', backref='user', lazy=True)
-    created_event = db.relationship('Event', backref='user', lazy=True)
+    __tablename__ = 'users'  # Table name in the database
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    full_name = db.Column(db.String(100), index=True, unique=True, nullable=False)  # User's full name
+    contact_number = db.Column(db.Integer, index=True, nullable=False)  # Contact number
+    email_id = db.Column(db.String(100), index=True, nullable=False)  # Email address
+    address = db.Column(db.String(100), index=True, nullable=False)  # Address
+    password_hash = db.Column(db.String(400), nullable=False)  # Hashed password
 
+    # Relationships
+    comments = db.relationship('Comment', backref='user', lazy=True)  # One-to-many relationship with comments
+    bookings = db.relationship('Booking', backref='user', lazy=True)  # One-to-many relationship with bookings
+    created_event = db.relationship('Event', backref='user', lazy=True)  # One-to-many relationship with events created
+
+    # Representation of the User object
     def __repr__(self):
         return f"Name: {self.full_name}"
 
+# Define the Event model
 class Event(db.Model):
-    __tablename__ = 'events'
-    id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(100), nullable=False)
-    event_category = db.Column(db.String(20), nullable=False)
-    event_thumbnail = db.Column(db.String(400))
-    event_summary = db.Column(db.String(200))
-    event_location = db.Column(db.String(200), nullable=False)
-    event_type  = db.Column(db.String(200), nullable=False)
-    start_date = db.Column(db.Date, nullable=False)
-    end_date = db.Column(db.Date, nullable=False)
-    event_description = db.Column(db.String(400))
-    start_time = db.Column(db.Time, nullable=False)
-    end_time = db.Column(db.Time, nullable=False)
-    ticket_price = db.Column(db.Float, nullable=False)
-    ticket_quantity = db.Column(db.Integer, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))
+    __tablename__ = 'events'  # Table name in the database
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    event_name = db.Column(db.String(100), nullable=False)  # Event name
+    event_category = db.Column(db.String(20), nullable=False)  # Event category
+    event_thumbnail = db.Column(db.String(400))  # Event thumbnail URL
+    event_summary = db.Column(db.String(200))  # Event summary
+    event_location = db.Column(db.String(200), nullable=False)  # Event location
+    event_type  = db.Column(db.String(200), nullable=False)  # Event type
+    start_date = db.Column(db.Date, nullable=False)  # Event start date
+    end_date = db.Column(db.Date, nullable=False)  # Event end date
+    event_description = db.Column(db.String(400))  # Event description
+    start_time = db.Column(db.Time, nullable=False)  # Event start time
+    end_time = db.Column(db.Time, nullable=False)  # Event end time
+    ticket_price = db.Column(db.Float, nullable=False)  # Ticket price
+    ticket_quantity = db.Column(db.Integer, nullable=False)  # Number of tickets available
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'))  # Foreign key to the user who created the event
 
-    # Relationship
-    comments = db.relationship('Comment', backref='event', lazy=True)
-    event_bookings = db.relationship('Booking', backref='event', lazy=True)
+    # Relationships
+    comments = db.relationship('Comment', backref='event', lazy=True)  # One-to-many relationship with comments
+    event_bookings = db.relationship('Booking', backref='event', lazy=True)  # One-to-many relationship with bookings
 
+    # Representation of the Event object
     def __repr__(self):
         return f"Name: {self.name}"
 
+# Define the Comment model
 class Comment(db.Model):
-    __tablename__ = 'comments'
-    id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(400), nullable=False)
-    posted_at = db.Column(db.DateTime, default=datetime.now, nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    __tablename__ = 'comments'  # Table name in the database
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    text = db.Column(db.String(400), nullable=False)  # Comment text
+    posted_at = db.Column(db.DateTime, default=datetime.now, nullable=False)  # Date and time when the comment was posted
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)  # Foreign key to the event
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Foreign key to the user who posted the comment
 
+    # Representation of the Comment object
     def __repr__(self):
         return f"Comment: {self.text}"
 
+# Define the Booking model
 class Booking(db.Model):
-    __tablename__ = 'bookings'
-    id = db.Column(db.Integer, primary_key=True)
-    event_name = db.Column(db.String(100), nullable=False)
-    event_price = db.Column(db.Float, nullable=False)
-    event_quantity = db.Column(db.Integer, nullable=False)
-    event_date = db.Column(db.Date, default=datetime.now, nullable=False)
-    start_time = db.Column(db.Time, nullable=False)
-    end_time = db.Column(db.Time, nullable=False)
-    event_location = db.Column(db.String(200), nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)
-    status_id = db.Column(db.Integer, db.ForeignKey('booking_statuses.id'), nullable=False)
+    __tablename__ = 'bookings'  # Table name in the database
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    event_name = db.Column(db.String(100), nullable=False)  # Name of the event
+    event_price = db.Column(db.Float, nullable=False)  # Price of the event ticket
+    event_quantity = db.Column(db.Integer, nullable=False)  # Number of tickets booked
+    event_date = db.Column(db.Date, default=datetime.now, nullable=False)  # Date of the event
+    start_time = db.Column(db.Time, nullable=False)  # Event start time
+    end_time = db.Column(db.Time, nullable=False)  # Event end time
+    event_location = db.Column(db.String(200), nullable=False)  # Location of the event
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)  # Foreign key to the user who made the booking
+    event_id = db.Column(db.Integer, db.ForeignKey('events.id'), nullable=False)  # Foreign key to the event
+    status_id = db.Column(db.Integer, db.ForeignKey('booking_statuses.id'), nullable=False)  # Foreign key to the booking status
 
+    # Representation of the Booking object
     def __repr__(self):
         return f"Booking for event_id: {self.event_id}, user_id: {self.user_id}"
 
+# Define the BookingStatus model
 class BookingStatus(db.Model):
-    __tablename__ = 'booking_statuses'
-    id = db.Column(db.Integer, primary_key=True)
-    status = db.Column(db.String(50), nullable=False)
+    __tablename__ = 'booking_statuses'  # Table name in the database
+    id = db.Column(db.Integer, primary_key=True)  # Primary key
+    status = db.Column(db.String(50), nullable=False)  # Booking status
 
-    # Relationship
-    booking = db.relationship('Booking', backref='status', lazy=True)
+    # Relationships
+    booking = db.relationship('Booking', backref='status', lazy=True)  # One-to-many relationship with bookings
 
+    # Representation of the BookingStatus object
     def __repr__(self):
         return f"Status: {self.status}"
-
-        return f"Comment: {self.text}"
